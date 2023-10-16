0a44698fc2789093524fdf4d1f256fb6f051e9ee
Islom Turgunov <147710629+ToshmirzaevNomonjon@users.noreply.github.com>
2023-10-16 18:49:26 +0500

    JS 5-dars
    27-guruh

---
+++ JS 5-dars/js/main.js
@@ -0,0 +1,60 @@
+/* Dom in JS */
+
+/*let headTag = document.head;*/
+/*let bodyTag = document.body;*/
+/*let havola = document.documentURI;*/
+/*let formalar = document.forms;*/
+/*let havolalar = document.links;*/
+/*console.log(havolalar);*/
+
+/* Events in JS (JS Hodisalar) */
+
+/*function meniBirMartaBos (){
+    alert("Siz battoni bir marta bosdingiz")
+}*/
+/*function meniIkkiMartaBos (){
+    alert("Siz battoni ikki marta bosing")
+}*/
+/*function  tanlash(event){
+    alert("Siz" + event.target.value + "ni tanladingiz");
+}*/
+/*function inputgaBosish(){
+    console.log("Inputga bosildi!")
+}*/
+/*function inputdanChiqish(){
+    console.log("Inputdan chqilidi")
+}*/
+/*function yozish(event){
+    console.log(event.target.value)
+}*/
+/*document.querySelector(".matnTegi").addEventListener('copy',function (event){
+    console.log("Diqqat! Diqqat! Sizdan " +event.target.value + "nomli xujjatlar kochirilyapti!");
+})*/
+
+function kirish(){
+    console.log("Hududga kirildi!")
+}
+function chiqish(){
+    console.log("Hududan chiqildi!")
+}
+function harakatlanish(e){
+    console.log(e.clientX,e.clientY);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
